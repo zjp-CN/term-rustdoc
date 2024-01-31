3b49bae7a822224c745101ce4c9765f9f42ae068
zjp <jiping_zhou@foxmail.com>
2024-01-31 17:15:07 +0800

    chore: mv Tag to its own module

--- src/tree/impls/show.rs
+++ src/tree/impls/show.rs
@@ -1,13 +1,13 @@
 use crate::{
-    tree::{IDMap, IdAsStr, ID},
+    tree::{IDMap, IdAsStr, Tag, ID},
     util::XString,
 };
-use ratatui::style::{Color, Style};
+use ratatui::style::Style;
 use std::{
     fmt::{self, Write},
     rc::Rc,
 };
-use termtree::{GlyphPalette, Tree};
+use termtree::Tree;
 
 macro_rules! icon {
     () => { ::termtree::GlyphPalette::new() };
@@ -157,157 +157,6 @@ macro_rules! names_node {
 
 pub fn show_ids(ids: &[ID]) -> impl '_ + Iterator<Item = DocTree> {
     ids.iter().map(|id| id.as_str().show())
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Tag {
-    Module,
-    Struct,
-    Union,
-    Enum,
-    Trait,
-    Function,
-    Constant,
-    Static,
-    TypeAlias,
-    Import,
-    MacroDecl,
-    MacroFunc,
-    MacroAttr,
-    MacroDerv,
-    Unknown,
-    NoImpls,
-    ImplInherent,
-    InherentImpls,
-    ImplTrait,
-    TraitImpls,
-    ImplAuto,
-    AutoImpls,
-    ImplBlanket,
-    BlanketImpls,
-    NoVariants, // Head for no variants
-    Variants,   // Head for variants
-    Variant,
-    NoFields,
-    Fields,
-    Field,
-    FieldsPrivate,
-    NoAssocOrImpls,
-    AssocTypes,
-    AssocType,
-    AssocConsts,
-    AssocConst,
-    AssocFns,
-    AssocFn,
-    Implementors,
-    Implementor,
-    Structs,
-    Unions,
-    Enums,
-    Traits,
-    Functions,
-    Constants,
-    Statics,
-    TypeAliass,
-    MacroDecls,
-    MacroFuncs,
-    MacroAttrs,
-    MacroDervs,
-}
-
-impl Tag {
-    pub fn style(self) -> Style {
-        let style = Style::default();
-        match self {
-            Tag::Module => style.fg(Color::LightRed),
-            Tag::Struct => style.fg(Color::Green),
-            Tag::Union => style.fg(Color::LightCyan),
-            Tag::Enum => style.fg(Color::Cyan),
-            Tag::Trait => style.fg(Color::Magenta),
-            Tag::Function => style.fg(Color::Rgb(60, 194, 200)),
-            Tag::Constant => style.fg(Color::Rgb(255, 138, 37)),
-            Tag::Static => style.fg(Color::Rgb(147, 72, 209)),
-            Tag::TypeAlias => style.fg(Color::Rgb(36, 14, 156)),
-            Tag::Import => style.fg(Color::Rgb(37, 143, 195)),
-            Tag::MacroDecl => style.fg(Color::Rgb(0, 180, 0)),
-            Tag::MacroFunc => style.fg(Color::Rgb(67, 227, 67)),
-            Tag::MacroAttr => style.fg(Color::Rgb(106, 233, 106)),
-            Tag::MacroDerv => style.fg(Color::Rgb(33, 222, 33)),
-            Tag::Unknown => style.fg(Color::DarkGray),
-            Tag::InherentImpls => style,
-            Tag::TraitImpls => style,
-            Tag::AutoImpls => style,
-            Tag::BlanketImpls => style,
-            _ => style,
-        }
-    }
-
-    pub fn glyph(self) -> GlyphPalette {
-        match self {
-            Tag::Module => icon!("[Mod]"),
-            Tag::Struct => icon!("[Struct]"),
-            Tag::Union => icon!("[Union]"),
-            Tag::Enum => icon!("[Enum]"),
-            Tag::Trait => icon!("[Trait]"),
-            Tag::Function => icon!("[Fn]"),
-            Tag::Constant => icon!("[Const]"),
-            Tag::Static => icon!("[Static]"),
-            Tag::TypeAlias => icon!("[type alias]"),
-            Tag::MacroDecl => icon!("[macro decl]"),
-            Tag::MacroFunc => icon!("[macro func]"),
-            Tag::MacroAttr => icon!("[macro attr]"),
-            Tag::MacroDerv => icon!("[macro derv]"),
-            Tag::ImplInherent => icon!("[inhrt]"),
-            Tag::ImplTrait => icon!("[trait]"),
-            Tag::ImplAuto => icon!("[auto]"),
-            Tag::ImplBlanket => icon!("[blkt]"),
-            Tag::Field => icon!("[field]"),
-            Tag::Variant => icon!("[variant]"),
-            Tag::AssocType => icon!("[assoc type]"),
-            Tag::AssocConst => icon!("[assoc constant]"),
-            Tag::AssocFn => icon!("[assoc fn]"),
-            _ => GlyphPalette::default(),
-        }
-    }
-
-    /// Show as a simple heading node with no need for contexts.
-    pub fn show(self) -> DocTree {
-        let text = match self {
-            Tag::NoImpls => "No Implementations!",
-            Tag::InherentImpls => "Inherent Impls",
-            Tag::TraitImpls => "Trait Impls",
-            Tag::AutoImpls => "Auto Impls",
-            Tag::BlanketImpls => "Blanket Impls",
-            Tag::NoVariants => "No Variants!",
-            Tag::Variants => "Variants",
-            Tag::NoFields => "No Fields!",
-            Tag::Fields => "Fields",
-            Tag::FieldsPrivate => "/* private fields */",
-            Tag::NoAssocOrImpls => "No Associated Items Or Implementors!",
-            Tag::AssocTypes => "Associated Types",
-            Tag::AssocConsts => "Associated Constants",
-            Tag::AssocFns => "Associated Functions",
-            Tag::Implementors => "Implementors",
-            Tag::Structs => "Structs",
-            Tag::Unions => "Unions",
-            Tag::Enums => "Enums",
-            Tag::Traits => "Traits",
-            Tag::Functions => "Functions",
-            Tag::Constants => "Constants",
-            Tag::Statics => "Statics",
-            Tag::TypeAliass => "Statics",
-            Tag::MacroDecls => "Macros - Declarative",
-            Tag::MacroFuncs => "Macros - Function",
-            Tag::MacroAttrs => "Macros - Attribute",
-            Tag::MacroDervs => "Macros - Derive",
-            // _ => format!("error for Tag `{self:?}`").leak(),
-            _ => unimplemented!(
-                "Tag `{self:?}` should reply on contexts like \
-                 name/path/type instead of plain text"
-            ),
-        };
-        DocTree::new(text.into(), self)
-    }
 }
 
 /// Tagged text including headings and nodes.
--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -6,11 +6,14 @@ mod impls;
 mod id;
 mod nodes;
 mod stats;
+mod tag;
 
 pub use id::{IDMap, IDs, IdAsStr, IdToID, IndexMap, PathMap, SliceToIds, ID};
-pub use impls::show::{DocTree, Show, Tag, Text, TreeLine, TreeLines};
+pub use impls::show::{DocTree, Show, Text, TreeLine, TreeLines};
 pub use nodes::{
     DConstant, DEnum, DFunction, DImpl, DMacroAttr, DMacroDecl, DMacroDerv, DMacroFunc, DModule,
     DStatic, DStruct, DTrait, DTypeAlias, DUnion,
 };
 pub use stats::{ImplCount, ImplCounts, ImplKind, ItemCount};
+pub use tag::Tag;
+

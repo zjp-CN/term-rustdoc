17219c686fde266d48bcf5de2856eb5308e87cdb
zjp <jiping_zhou@foxmail.com>
2024-02-14 11:36:23 +0800

    refactor: LinkedRegions

--- src/bin/ui/scrollable/markdown/region.rs
+++ src/bin/ui/scrollable/markdown/region.rs
@@ -1,9 +1,10 @@
 use smallvec::SmallVec;
+use term_rustdoc::util::{hashmap, HashMap, XString};
 
 /// The selected texts will be rendered with original fg but grey bg.
 ///
 /// NOTE: the region is continuous across lines.
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Hash, PartialEq, Eq)]
 pub struct SelectedRegion {
     row_start: usize,
     row_end: usize,
@@ -11,22 +12,37 @@ pub struct SelectedRegion {
     col_end: usize,
 }
 
-/// When the cursor or selection falls into this region,
-/// the regions in targets will be into the same background color.
-pub struct Linkage {
-    selected: SelectedRegion,
+#[derive(Debug, Hash, PartialEq, Eq)]
+#[allow(dead_code)]
+pub enum RegionTag {
+    // /// A continuous region on screen.
+    // OnScreen(SelectedRegion),
+    /// A string key like headings or impl or key of a footnote.
+    Heading(u8, XString),
+    FootNote(XString),
+    FootNoteSrc(XString),
+    /// A referencd link id
+    Link(usize),
+    LinkSrc(usize),
+}
+
+pub struct TargetRegion {
     targets: SmallVec<[SelectedRegion; 1]>,
 }
 
 /// Regions that bidirect to each other.
+/// When the cursor or selection falls into a region,
+/// the regions in targets will be into the same background color.
 pub struct LinkedRegions {
-    regions: Vec<Linkage>,
+    tag: HashMap<RegionTag, TargetRegion>,
+    // regions: Vec<(SelectedRegion, TargetRegion)>,
 }
 
 impl LinkedRegions {
     pub fn new() -> LinkedRegions {
         LinkedRegions {
-            regions: Vec::with_capacity(8),
+            tag: hashmap(8),
+            // regions: Vec::with_capacity(4),
         }
     }
 }

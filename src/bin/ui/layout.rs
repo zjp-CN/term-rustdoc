fb53ad9b1d4a698e4db103dfab0ab84868c1f8c2
zjp <jiping_zhou@foxmail.com>
2024-02-16 17:08:27 +0800

    chore: mv layout to own module

---
+++ src/bin/ui/layout.rs
@@ -0,0 +1,85 @@
+use super::{panel::SET, Page, Panel, Surround};
+use ratatui::{
+    prelude::{Constraint, Direction, Layout, Rect},
+    widgets::{Block, BorderType, Borders},
+};
+
+impl Page {
+    fn layout(&self) -> Layout {
+        let outline_width = self.outline.display.max_width + 1;
+        Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([
+                Constraint::Length(outline_width),
+                Constraint::Min(0),
+                Constraint::Percentage(10),
+            ])
+    }
+
+    /// This is called in Widget's render method because inner widgets don't implement
+    /// Widget, since the areas they draw are updated only from here, not from Widget trait.
+    pub(super) fn update_area(&mut self, full: Rect) {
+        // skip updating since the size is the same
+        if self.area == full {
+            return;
+        }
+
+        self.update_area_inner(full);
+    }
+
+    /// Force update Page inner layout.
+    ///
+    /// `full` usually should be the full screen area or Page area.
+    pub(super) fn update_area_inner(&mut self, full: Rect) {
+        // layout
+        self.area = full;
+        let layout = self.layout().split(full);
+
+        // border
+        let outline_border = Block::new()
+            .borders(Borders::RIGHT)
+            .border_type(BorderType::Thick);
+        self.outline.border = Surround {
+            block: if matches!(self.current, None | Some(Panel::Outline)) {
+                outline_border.style(SET)
+            } else {
+                outline_border
+            },
+            area: layout[0],
+        };
+        let outline_area = self.outline.border.inner();
+        self.content.border = Surround {
+            block: Block::new(),
+            area: layout[1],
+        };
+        let content_area = self.content.border.inner();
+
+        // display.area
+        self.outline.display.area = outline_area;
+        // self.outline.display.cursor = outline_area.y;
+        let outline_max_width = self.outline.display.max_width;
+        if outline_area.width < outline_max_width {
+            warn!(
+                outline_area.width,
+                outline_max_width,
+                "Outline width exceeds the area width, so lines may be truncated."
+            );
+        }
+
+        self.content.display.area = content_area;
+        self.content.display.max_width = content_area.width;
+
+        if let Some(&navi_outer_area) = layout.get(2) {
+            self.navi.border = Surround {
+                block: Block::new()
+                    .borders(Borders::LEFT)
+                    .border_type(BorderType::Thick),
+                area: navi_outer_area,
+            };
+            self.navi.display.area = self.navi.border.inner();
+        }
+
+        // auto update content when screen size changes
+        self.update_content();
+    }
+}
--- src/bin/ui/mod.rs
+++ src/bin/ui/mod.rs
@@ -1,17 +1,21 @@
-use self::panel::{Panel, SET};
-use self::scrollable::{ScrollHeading, ScrollText, ScrollTreeLines, Scrollable};
+use self::{
+    panel::Panel,
+    scrollable::{ScrollHeading, ScrollText, ScrollTreeLines, Scrollable},
+};
 use crate::{app::App, Result};
 use ratatui::{
-    prelude::{Buffer, Constraint, Direction, Frame, Layout, Rect, Widget},
-    widgets::{Block, BorderType, Borders},
+    prelude::{Buffer, Frame, Rect, Widget},
+    widgets::Block,
 };
 use term_rustdoc::tree::{CrateDoc, TreeLines};
+
+mod layout;
+mod panel;
 
 /// fold/expand a tree view
 mod page_fold;
 /// scroll up/down behavior and with what offset
 mod page_scroll;
-mod panel;
 /// Scrollable widget
 mod scrollable;
 
@@ -56,84 +60,6 @@ impl Page {
             Some(Panel::Outline) => self.outline_fold_expand_toggle(),
             _ => {}
         }
-    }
-
-    fn layout(&self) -> Layout {
-        let outline_width = self.outline.display.max_width + 1;
-        Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints([
-                Constraint::Length(outline_width),
-                Constraint::Min(0),
-                Constraint::Percentage(10),
-            ])
-    }
-
-    /// This is called in Widget's render method because inner widgets don't implement
-    /// Widget, since the areas they draw are updated only from here, not from Widget trait.
-    fn update_area(&mut self, full: Rect) {
-        // skip updating since the size is the same
-        if self.area == full {
-            return;
-        }
-
-        self.update_area_inner(full);
-    }
-
-    /// Force update Page inner layout.
-    ///
-    /// `full` usually should be the full screen area or Page area.
-    fn update_area_inner(&mut self, full: Rect) {
-        // layout
-        self.area = full;
-        let layout = self.layout().split(full);
-
-        // border
-        let outline_border = Block::new()
-            .borders(Borders::RIGHT)
-            .border_type(BorderType::Thick);
-        self.outline.border = Surround {
-            block: if matches!(self.current, None | Some(Panel::Outline)) {
-                outline_border.style(SET)
-            } else {
-                outline_border
-            },
-            area: layout[0],
-        };
-        let outline_area = self.outline.border.inner();
-        self.content.border = Surround {
-            block: Block::new(),
-            area: layout[1],
-        };
-        let content_area = self.content.border.inner();
-
-        // display.area
-        self.outline.display.area = outline_area;
-        // self.outline.display.cursor = outline_area.y;
-        let outline_max_width = self.outline.display.max_width;
-        if outline_area.width < outline_max_width {
-            warn!(
-                outline_area.width,
-                outline_max_width,
-                "Outline width exceeds the area width, so lines may be truncated."
-            );
-        }
-
-        self.content.display.area = content_area;
-        self.content.display.max_width = content_area.width;
-
-        if let Some(&navi_outer_area) = layout.get(2) {
-            self.navi.border = Surround {
-                block: Block::new()
-                    .borders(Borders::LEFT)
-                    .border_type(BorderType::Thick),
-                area: navi_outer_area,
-            };
-            self.navi.display.area = self.navi.border.inner();
-        }
-
-        // auto update content when screen size changes
-        self.update_content();
     }
 }
 

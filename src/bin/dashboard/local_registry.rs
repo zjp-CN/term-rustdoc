344036c96fd5925aaabe2a88c2ff137c1e31ada8
zjp <jiping_zhou@foxmail.com>
2024-02-19 11:48:25 +0800

    refactor: find the pkg's Cargo.toml via Path::exists instead of WalkDir
    Only search pkgs under registry_src dir with Cargo.toml in pkg's root
    like `registry_src/pkgname-version/Cargo.toml`.
    Also note that if IO error happens or version is unable to parse, the
    pkg is filtered out.

--- src/bin/dashboard/local_registry.rs
+++ src/bin/dashboard/local_registry.rs
@@ -1,14 +1,13 @@
 use crate::Result;
-use color_eyre::eyre::Ok;
 use itertools::Itertools;
 use regex::Regex;
 use semver::Version;
 use std::{
     fs,
     path::{Component, Path, PathBuf},
+    time::SystemTime,
 };
 use term_rustdoc::util::XString;
-use walkdir::WalkDir;
 
 fn latest_registry() -> Result<Option<PathBuf>> {
     let mut cargo = home::cargo_home()?;
@@ -23,21 +22,37 @@ fn latest_registry() -> Result<Option<PathBuf>> {
     Ok(entries.pop().map(|v| v.1))
 }
 
-pub fn all_pkgs_in_latest_registry(registry_src: &Path) -> Result<Vec<PkgNameVersion>> {
-    let mut cargo_tomls: Vec<_> = WalkDir::new(registry_src)
-        .max_depth(2)
-        .into_iter()
-        .filter_map(|f| {
-            f.ok().and_then(|f| {
-                (f.file_name() == "Cargo.toml")
-                    .then(|| PkgNameVersion::new(f.into_path()))
-                    .flatten()
-            })
-        })
-        .collect();
-    cargo_tomls.sort_unstable_by(|a, b| (&*a.name, &a.version).cmp(&(&*b.name, &b.version)));
-    cargo_tomls.shrink_to_fit();
-    Ok(cargo_tomls)
+fn find_pkgs(registry_src: &Path) -> Vec<PkgNameVersion> {
+    match fs::read_dir(registry_src) {
+        Ok(entries) => {
+            entries
+                .filter_map(|entry| {
+                    let dir = entry.ok()?;
+                    if dir.file_type().ok()?.is_dir() {
+                        let mut pkg_path = dir.path();
+                        // check if the pkg contains Cargo.toml in its root
+                        pkg_path.push("Cargo.toml");
+                        if pkg_path.exists() {
+                            pkg_path.pop();
+                            return PkgNameVersion::new_pkg(pkg_path);
+                        }
+                    }
+                    None
+                })
+                .collect()
+        }
+        Err(err) => {
+            error!("Failed to read `{}` dir:\n{err}", registry_src.display());
+            Vec::new()
+        }
+    }
+}
+
+pub fn all_pkgs_in_latest_registry(registry_src: &Path) -> Vec<PkgNameVersion> {
+    let mut pkgs = find_pkgs(registry_src);
+    pkgs.sort_unstable_by(|a, b| (&*a.name, &a.version).cmp(&(&*b.name, &b.version)));
+    pkgs.shrink_to_fit();
+    pkgs
 }
 
 fn pkg_name_version(path: &Path) -> Option<Component<'_>> {
@@ -66,7 +81,7 @@ impl LocalRegistry {
         let Some(path) = latest_registry()? else {
             return Ok(Self::default());
         };
-        let pkgs = all_pkgs_in_latest_registry(&path)?;
+        let pkgs = all_pkgs_in_latest_registry(&path);
         Ok(LocalRegistry { pkgs, path })
     }
 
@@ -74,7 +89,7 @@ impl LocalRegistry {
         let Some(path) = latest_registry()? else {
             return Ok(Self::default());
         };
-        let pkgs = lastest_pkgs_in_latest_registry(&path)?;
+        let pkgs = lastest_pkgs_in_latest_registry(&path);
         Ok(LocalRegistry { pkgs, path })
     }
 
@@ -89,35 +104,42 @@ impl LocalRegistry {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PkgNameVersion {
+    /// Pkg name from dir name.
     name: XString,
+    /// Pkg version from dir name.
+    ver_str: XString,
+    /// Pkg version parsed from ver_str.
     version: Version,
+    /// The pkg dir path not including Cargo.toml.
     path: PathBuf,
+    /// The last modified time for pkg dir.
+    modified: SystemTime,
 }
 
 impl Default for PkgNameVersion {
     fn default() -> Self {
-        let (name, path) = Default::default();
+        let (name, ver_str, path) = Default::default();
         PkgNameVersion {
             name,
+            ver_str,
             version: Version::new(0, 0, 0),
             path,
+            modified: SystemTime::now(),
         }
     }
 }
 
 impl PkgNameVersion {
-    fn new(path: PathBuf) -> Option<Self> {
-        if let Some(c) = pkg_name_version(&path) {
-            let s = c.as_os_str().to_str()?;
-            let (name, version) = get_pkg_name(s)?;
-            Some(PkgNameVersion {
-                name,
-                version,
-                path,
-            })
-        } else {
-            None
-        }
+    fn new_pkg(pkg_path: PathBuf) -> Option<Self> {
+        let modified = pkg_path.metadata().ok()?.modified().ok()?;
+        let (name, ver, version) = get_pkg_name(pkg_path.file_name()?.to_str()?)?;
+        Some(PkgNameVersion {
+            name,
+            ver_str: ver,
+            version,
+            path: pkg_path,
+            modified,
+        })
     }
 
     pub fn name(&self) -> &str {
@@ -129,20 +151,21 @@ thread_local! {
     static RE: Regex = Regex::new(r"-\d+\.\d+\.\d+.*?$").unwrap();
 }
 
-fn get_pkg_name(name_ver: &str) -> Option<(XString, Version)> {
+fn get_pkg_name(name_ver: &str) -> Option<(XString, XString, Version)> {
     RE.with(|re| {
         re.find(name_ver).and_then(|m| {
             let start = m.start();
-            name_ver[start + 1..m.end()]
-                .parse()
+            let ver = &name_ver[start + 1..m.end()];
+            ver.parse()
                 .ok()
-                .map(|version| (name_ver[..start].into(), version))
+                .map(|version| (name_ver[..start].into(), ver.into(), version))
         })
     })
 }
 
-pub fn lastest_pkgs_in_latest_registry(registry_src: &Path) -> Result<Vec<PkgNameVersion>> {
-    let mut paths: Vec<_> = all_pkgs_in_latest_registry(registry_src)?
+/// Pkgs with lastest version.
+pub fn lastest_pkgs_in_latest_registry(registry_src: &Path) -> Vec<PkgNameVersion> {
+    let mut pkgs: Vec<_> = all_pkgs_in_latest_registry(registry_src)
         .into_iter()
         .group_by(|pkg| pkg.name.clone())
         .into_iter()
@@ -152,16 +175,16 @@ pub fn lastest_pkgs_in_latest_registry(registry_src: &Path) -> Result<Vec<PkgNam
                 .unwrap()
         })
         .collect();
-    paths.shrink_to_fit();
-    Ok(paths)
+    pkgs.shrink_to_fit();
+    pkgs
 }
 
 #[test]
 fn local_registry_pkgs() -> Result<()> {
     let registry_src = latest_registry()?.unwrap();
-    // let paths = all_pkgs_in_latest_registry(&registry_src)?;
+    // let paths = all_pkgs_in_latest_registry(&registry_src);
     // dbg!(&paths);
-    let pkgs = lastest_pkgs_in_latest_registry(&registry_src)?;
+    let pkgs = lastest_pkgs_in_latest_registry(&registry_src);
     dbg!(pkgs.iter().map(|p| &p.path).collect::<Vec<_>>());
     Ok(())
 }

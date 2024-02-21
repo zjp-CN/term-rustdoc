148ecbe0ede2cd02cba309e73436f3e81d36a90a
zjp <jiping_zhou@foxmail.com>
2024-02-21 17:07:27 +0800

    chore: allow ansi color in logging

--- src/bin/logger.rs
+++ src/bin/logger.rs
@@ -5,7 +5,7 @@ pub fn init() -> Result<()> {
     let file = File::create("./target/term_rustdoc.log")?;
     tracing_subscriber::fmt()
         .with_writer(file)
-        .with_ansi(false)
+        // .with_ansi(false)
         .try_init()
         .map_err(|_| err!("logger init failed: maybe there is another initializer?"))?;
     info!("logging initialized and the program starts");

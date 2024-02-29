c7f0c895bb30cad680388566c44469f474a10cc0
zjp <jiping_zhou@foxmail.com>
2024-02-29 20:22:46 +0800

    fix: render_line when it's still available to stopping text

--- src/bin/ui/scrollable/generics.rs
+++ src/bin/ui/scrollable/generics.rs
@@ -1,6 +1,7 @@
 use ratatui::{buffer::Buffer, style::Style};
 use std::ops::Deref;
 use term_rustdoc::{tree::TreeLine, util::XString};
+use unicode_width::UnicodeWidthStr;
 
 pub trait Lines: Deref<Target = [Self::Line]> {
     type Line: LineState;
@@ -27,7 +28,9 @@ impl LineState for TreeLine {
         self.id == *state
     }
 }
-// worry too much about what features they should pick. If you're unsure, we suggest\n
+
+// Render a line as much as possible. Stop when the width is not enough, but still try to
+// write the stoping text in the remaining width.
 pub fn render_line<'t, I>(line: I, buf: &mut Buffer, mut x: u16, y: u16, width: usize) -> usize
 where
     I: IntoIterator<Item = (&'t str, Style)>,
@@ -35,8 +38,23 @@ where
     let mut used_width = 0usize;
     for (text, style) in line {
         // stop rendering once it hits the end of width
-        if used_width > width {
-            info!("{text:?} truncated in row {y} col {x} (used {used_width}, maximum {width})");
+        let text_width = text.width();
+        if used_width + text_width > width {
+            if let Some(rest) = width.checked_sub(used_width).filter(|w| *w > 0) {
+                let (succeed, fail) = if let Some(text) = text.get(..rest) {
+                    let (x_pos, _) = buf.set_stringn(x, y, text, width, style);
+                    used_width += x_pos.saturating_sub(x) as usize;
+                    x = x_pos;
+                    (rest, text_width.saturating_sub(rest))
+                } else {
+                    (0, text_width)
+                };
+                warn!(
+                    "{text:?} truncated in row {y} col {x} and possibly partially written. \
+                     (used {used_width}, the next text_width {text_width} \
+                     with {succeed} written {fail} not written, maximum {width})",
+                );
+            }
             return used_width;
         }
         let (x_pos, _) = buf.set_stringn(x, y, text, width, style);
@@ -60,9 +78,23 @@ pub fn render_line_fill_gap<'t, I>(
     let mut used_width = 0usize;
     for text in line {
         // stop rendering once it hits the end of width
-        if used_width > width {
-            info!("{text:?} truncated in row {y} col {x} (used {used_width}, maximum {width})");
-            return;
+        let text_width = text.width();
+        if used_width + text_width > width {
+            if let Some(rest) = width.checked_sub(used_width).filter(|w| *w > 0) {
+                let (succeed, fail) = if let Some(text) = text.get(..rest) {
+                    let (x_pos, _) = buf.set_stringn(x, y, text, width, style);
+                    used_width += x_pos.saturating_sub(x) as usize;
+                    x = x_pos;
+                    (rest, text_width.saturating_sub(rest))
+                } else {
+                    (0, text_width)
+                };
+                warn!(
+                    "{text:?} truncated in row {y} col {x} and possibly partially written. \
+                     (used {used_width}, the next text_width {text_width} \
+                     with {succeed} written {fail} not written, maximum {width})",
+                );
+            }
         }
         let (x_pos, _) = buf.set_stringn(x, y, text, width, style);
         used_width += x_pos.saturating_sub(x) as usize;

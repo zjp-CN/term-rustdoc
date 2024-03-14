1634310f54ce34f534a373bbbf72a870e9ca9cde
zjp <jiping_zhou@foxmail.com>
2024-03-14 16:55:54 +0800

    test: apply DisplaySlice instead of Debug for slice

--- tests/parse-json-docs/fn_item_decl.rs
+++ tests/parse-json-docs/fn_item_decl.rs
@@ -1,4 +1,4 @@
-use crate::{doc, shot, snap};
+use crate::{doc, shot};
 use term_rustdoc::{
     decl::item_str,
     tree::{DModule, IDMap},
@@ -12,22 +12,20 @@ fn fn_items() {
     for fn_ in &dmod.functions {
         fns_str.push(item_str(&fn_.id, map));
     }
-    snap!(fns_str, @r###"
-    [
-        "pub fn func_dyn_trait(d: &(dyn ATrait + Send + Sync)) -> &dyn ATrait",
-        "pub fn func_dyn_trait2(_: Box<dyn ATrait + Send + Sync>)",
-        "pub fn func_fn_pointer_impl_trait(f: fn(*mut u8) -> *const u8) -> impl Copy + Fn(*mut u8) -> *const u8",
-        "pub fn func_hrtb<T: ATraitWithGAT>() where for<'a> <T as ATraitWithGAT>::Assoc<'a>: Copy",
-        "pub fn func_lifetime_bounds<'a, 'b: 'a>() where 'a: 'b",
-        "pub fn func_primitive(s: &str) -> usize",
-        "pub fn func_qualified_path<'a, I: Iterator>(iter: I) -> Option<I::Item> where I::Item: 'a + Debug + Iterator<Item = ()> + ATraitWithGAT<Assoc<'a> = ()>",
-        "pub fn func_trait_bounds<T>() where T: Clone + Copy",
-        "pub fn func_tuple_array_slice<'a, 'b>(a: &'a u8, b: &'b mut [u8; 8], _: &'b mut (dyn 'a + ATrait)) -> (&'a u8, &'b mut [u8; 8])",
-        "pub fn func_with_1arg(_: FieldsNamedStruct)",
-        "pub fn func_with_1arg_and_ret(f: FieldsNamedStruct) -> AUnitEnum",
-        "pub fn func_with_const<T: Copy, const N: usize>(t: T) -> [T; N]",
-        "pub fn func_with_no_args()",
-    ]
+    shot!(DisplaySlice(&fns_str), @r###"
+    pub fn func_dyn_trait(d: &(dyn ATrait + Send + Sync)) -> &dyn ATrait
+    pub fn func_dyn_trait2(_: Box<dyn ATrait + Send + Sync>)
+    pub fn func_fn_pointer_impl_trait(f: fn(*mut u8) -> *const u8) -> impl Copy + Fn(*mut u8) -> *const u8
+    pub fn func_hrtb<T: ATraitWithGAT>() where for<'a> <T as ATraitWithGAT>::Assoc<'a>: Copy
+    pub fn func_lifetime_bounds<'a, 'b: 'a>() where 'a: 'b
+    pub fn func_primitive(s: &str) -> usize
+    pub fn func_qualified_path<'a, I: Iterator>(iter: I) -> Option<I::Item> where I::Item: 'a + Debug + Iterator<Item = ()> + ATraitWithGAT<Assoc<'a> = ()>
+    pub fn func_trait_bounds<T>() where T: Clone + Copy
+    pub fn func_tuple_array_slice<'a, 'b>(a: &'a u8, b: &'b mut [u8; 8], _: &'b mut (dyn 'a + ATrait)) -> (&'a u8, &'b mut [u8; 8])
+    pub fn func_with_1arg(_: FieldsNamedStruct)
+    pub fn func_with_1arg_and_ret(f: FieldsNamedStruct) -> AUnitEnum
+    pub fn func_with_const<T: Copy, const N: usize>(t: T) -> [T; N]
+    pub fn func_with_no_args()
     "###);
 }
 
@@ -56,15 +54,13 @@ fn methods() {
             fns_str.push(item_str(&fn_.id, map));
         }
     }
-    snap!(fns_str, @r###"
-    [
-        "pub fn by_rc(self: Rc<Self>)",
-        "pub fn by_ref(&self)",
-        "pub fn by_ref_mut(&mut self)",
-        "pub fn consume(self)",
-        "pub fn new() -> Self",
-        "fn return_assoc(&self) -> Self::Assoc<'_>",
-    ]
+    shot!(DisplaySlice(&fns_str), @r###"
+    pub fn by_rc(self: Rc<Self>)
+    pub fn by_ref(&self)
+    pub fn by_ref_mut(&mut self)
+    pub fn consume(self)
+    pub fn new() -> Self
+    fn return_assoc(&self) -> Self::Assoc<'_>
     "###);
 }
 

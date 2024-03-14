6de9a51984c526099c93b3e308db66d73bb1eb14
zjp <jiping_zhou@foxmail.com>
2024-03-14 13:33:27 +0800

    refactor: Parse trait in decl

--- src/decl/fn_.rs
+++ src/decl/fn_.rs
@@ -1,3 +1,4 @@
+use super::Parse;
 use crate::{
     tree::IDMap,
     type_name::{fn_decl, fn_header, generics},
@@ -5,46 +6,37 @@ use crate::{
 use rustdoc_types::{Function, ItemEnum, Visibility};
 use std::fmt::Write;
 
-fn vis(v: &Visibility, buf: &mut String) {
-    match v {
-        Visibility::Public => buf.push_str("pub "),
-        Visibility::Default => (),
-        Visibility::Crate => buf.push_str("pub(crate) "),
-        Visibility::Restricted { path, .. } => write!(buf, "pub({path}) ").unwrap(),
-    };
-}
+impl Parse for Function {
+    fn parse(&self, v: &Visibility, fname: &str) -> String {
+        let Function {
+            decl,
+            generics: g,
+            header,
+            ..
+        } = self;
 
-fn parse_fn(
-    v: &Visibility,
-    fname: &str,
-    Function {
-        decl,
-        generics: g,
-        header,
-        ..
-    }: &Function,
-) -> String {
-    let mut buf = String::with_capacity(128);
-    vis(v, &mut buf);
-    fn_header(header, &mut buf);
-    buf.push_str("fn ");
-    buf.push_str(fname);
-    let (def, where_) = generics(g);
-    if let Some(def) = &def {
-        write!(buf, "<{def}>").unwrap();
+        let mut buf = String::with_capacity(128);
+        super::vis(v, &mut buf);
+        fn_header(header, &mut buf);
+        buf.push_str("fn ");
+        buf.push_str(fname);
+        let (def, where_) = generics(g);
+        if let Some(def) = &def {
+            write!(buf, "<{def}>").unwrap();
+        }
+        fn_decl(decl, &mut buf);
+        if let Some(where_) = &where_ {
+            write!(buf, " where {where_}").unwrap();
+        }
+        buf
     }
-    fn_decl(decl, &mut buf);
-    if let Some(where_) = &where_ {
-        write!(buf, " where {where_}").unwrap();
-    }
-    buf
 }
 
 pub fn fn_item(id: &str, map: &IDMap) -> String {
     if let Some(item) = map.get_item(id) {
         if let ItemEnum::Function(f) = &item.inner {
             let fname = item.name.as_deref().unwrap_or("");
-            return parse_fn(&item.visibility, fname, f);
+            return f.parse(&item.visibility, fname);
         }
     }
     String::new()
--- src/decl/mod.rs
+++ src/decl/mod.rs
@@ -1,2 +1,19 @@
+use rustdoc_types::Visibility;
+use std::fmt::Write;
+
 mod fn_;
 pub use fn_::fn_item;
+
+fn vis(v: &Visibility, buf: &mut String) {
+    match v {
+        Visibility::Public => buf.push_str("pub "),
+        Visibility::Default => (),
+        Visibility::Crate => buf.push_str("pub(crate) "),
+        Visibility::Restricted { path, .. } => _ = write!(buf, "pub({path}) "),
+    };
+}
+
+/// Parse Item as String.
+trait Parse {
+    fn parse(&self, v: &Visibility, fname: &str) -> String;
+}

98ec03cbe801f087273174af08d0a9dbf3cab68b
zjp <jiping_zhou@foxmail.com>
2024-03-15 09:50:31 +0800

    chore: put most likely cases first in struct formating

--- src/decl/struct_.rs
+++ src/decl/struct_.rs
@@ -26,30 +26,6 @@ impl Format for Struct {
         let (def, where_) = generics(g);
         let b = &mut buf;
         match (kind, &def, &where_) {
-            (StructKind::Unit, None, None) => b.push(';'),
-            (StructKind::Unit, None, Some(w)) => _ = write!(b, "\nwhere\n{w};"),
-            (StructKind::Unit, Some(d), None) => _ = write!(b, "<{d}>;"), // very unlikely
-            (StructKind::Unit, Some(d), Some(w)) => _ = write!(b, "<{d}>\nwhere\n{w};"),
-            (StructKind::Tuple(t), None, None) => {
-                b.push('(');
-                push_tuple_fields(t, map, b);
-                b.push(')');
-            }
-            (StructKind::Tuple(t), None, Some(w)) => {
-                b.push('(');
-                push_tuple_fields(t, map, b);
-                _ = write!(b, ")\nwhere\n{w};");
-            }
-            (StructKind::Tuple(t), Some(d), None) => {
-                _ = write!(b, "<{d}>(");
-                push_tuple_fields(t, map, b);
-                b.push_str(");");
-            }
-            (StructKind::Tuple(t), Some(d), Some(w)) => {
-                _ = write!(b, "<{d}>(");
-                push_tuple_fields(t, map, b);
-                _ = write!(b, ")\nwhere\n{w};");
-            }
             (
                 StructKind::Plain {
                     fields,
@@ -58,17 +34,6 @@ impl Format for Struct {
                 None,
                 None,
             ) => write_body(b, fields, map, *fields_stripped),
-            (
-                StructKind::Plain {
-                    fields,
-                    fields_stripped,
-                },
-                None,
-                Some(w),
-            ) => {
-                _ = write!(b, "\nwhere\n{w}");
-                write_body(b, fields, map, *fields_stripped);
-            }
             (
                 StructKind::Plain {
                     fields,
@@ -91,6 +56,43 @@ impl Format for Struct {
                 _ = write!(b, "<{d}>\nwhere\n{w}");
                 write_body(b, fields, map, *fields_stripped);
             }
+            (StructKind::Tuple(t), None, None) => {
+                b.push('(');
+                push_tuple_fields(t, map, b);
+                b.push(')');
+            }
+            (StructKind::Tuple(t), Some(d), None) => {
+                _ = write!(b, "<{d}>(");
+                push_tuple_fields(t, map, b);
+                b.push_str(");");
+            }
+            (StructKind::Tuple(t), Some(d), Some(w)) => {
+                _ = write!(b, "<{d}>(");
+                push_tuple_fields(t, map, b);
+                _ = write!(b, ")\nwhere\n{w};");
+            }
+            (StructKind::Unit, None, None) => b.push(';'),
+
+            // less likely
+            (
+                StructKind::Plain {
+                    fields,
+                    fields_stripped,
+                },
+                None,
+                Some(w),
+            ) => {
+                _ = write!(b, "\nwhere\n{w}");
+                write_body(b, fields, map, *fields_stripped);
+            }
+            (StructKind::Tuple(t), None, Some(w)) => {
+                b.push('(');
+                push_tuple_fields(t, map, b);
+                _ = write!(b, ")\nwhere\n{w};");
+            }
+            (StructKind::Unit, Some(d), None) => _ = write!(b, "<{d}>;"),
+            (StructKind::Unit, None, Some(w)) => _ = write!(b, "\nwhere\n{w};"),
+            (StructKind::Unit, Some(d), Some(w)) => _ = write!(b, "<{d}>\nwhere\n{w};"),
         }
         buf
     }

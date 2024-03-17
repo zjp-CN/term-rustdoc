94461a4391be1f152abb64aa16ac7213ff2efb9f
zjp <jiping_zhou@foxmail.com>
2024-03-17 23:22:41 +0800

    feat: add Tag::{str_len, write_to_string}, StyledType::to_string
    also chore for simplifying to_str!

--- src/type_name/style/mod.rs
+++ src/type_name/style/mod.rs
@@ -19,22 +19,64 @@ impl StyledType {
     fn write_format(&mut self, fmt: impl Format) {
         fmt.format(self);
     }
+
+    #[allow(clippy::inherent_to_string)]
+    fn to_string(&self) -> String {
+        let cap = self.inner.iter().map(Tag::str_len).sum::<usize>();
+        let mut buf = String::with_capacity(cap);
+        for tag in &self.inner {
+            tag.write_to_string(&mut buf)
+        }
+        buf
+    }
 }
 
 /// Rendering tag which represents color, style and metadata that are used to jump.
+#[derive(Clone, Debug)]
 pub enum Tag {
     /// A path to an item that is usually carries an ID.
     /// We use the ID to jump to another item.
     /// The Path do not include generics.
-    /// A Path ID tag is conjuction with with its Name tag.
+    /// A Path ID tag is conjuction with its Name tag.
     Path(ID),
     /// A Name is a short path an ID can refer to or somthing not statically known.
     /// E.g. short path/type name, function argument, name for field, variant and generics etc.
     Name(XString),
     Symbol(Symbol),
     Decl(Decl),
+    /// The scope id is in conjuction with a Name.
+    /// `pub(scope)` is composed of [`Vis::PubScope`], [`Punctuation::ParenthesisStart`],
+    /// [`Tag::Name`] and [`Punctuation::ParenthesisEnd`].
     PubScope(ID),
+    /// In conjuction with [`Abi`].
+    /// `extern "other_abi" ` is composed of [`Abi::Other`], [`Punctuation::Quote`],
+    /// [`Tag::UnusualAbi`], [`Punctuation::Quote`] and [`Punctuation::WhiteSpace`].
     UnusualAbi(XString),
+}
+
+impl Tag {
+    pub fn write_to_string(&self, buf: &mut String) {
+        match self {
+            Tag::Path(_) => (),
+            Tag::Name(s) => buf.push_str(s),
+            Tag::Symbol(s) => buf.push_str(s.to_str()),
+            Tag::Decl(s) => buf.push_str(s.to_str()),
+            Tag::PubScope(_) => (),
+            Tag::UnusualAbi(s) => buf.push_str(s),
+        }
+    }
+    pub fn str_len(&self) -> usize {
+        match self {
+            // a path uses name len instead of id len
+            Tag::Path(_) => 0,
+            Tag::Name(s) => s.len(),
+            Tag::Symbol(s) => s.str_len(),
+            Tag::Decl(s) => s.str_len(),
+            // `pub(scope)`: scope use name len rather than id len, but we should count `pub()` here
+            Tag::PubScope(_) => 0,
+            Tag::UnusualAbi(s) => s.len(),
+        }
+    }
 }
 
 // macro MWE for interspersing fieldless variants with field-carrying ones:
@@ -46,73 +88,72 @@ macro_rules! to_str {
         $(#[$em:meta])*
         $vis:vis enum $e:ident { $($t:tt)+ }
     ) => {
-        to_str!(@impl [def {$(#[$em])*} {$vis} $e {}] [to_str {}] [str_len {}] : $($t)+);
+        to_str!(@impl [def {$(#[$em])* $vis} $e {}] [to_str {}] [str_len {}] : $($t)+);
     };
     // expand token trees
     (@impl
-     [def {$(#[$em:meta])*} {$vis:vis} $e:ident $({ $($vars:tt)* })*]
+     [def {$(#[$em:meta])* $vis:vis} $e:ident { $($vars:tt)* }]
      [to_str  { $($b1:tt)* }]
      [str_len { $($b2:tt)* }] :
     ) => {
-        #[derive(Clone, Copy, Debug)] $(#[$em])* $vis enum $e {
-            $( $($vars)* )*
-        }
+        #[derive(Clone, Copy, Debug)] $(#[$em])*
+        $vis enum $e { $($vars)* }
         impl $e {
-            pub fn to_str(self) -> &'static str {
+            pub const fn to_str(self) -> &'static str {
                 match self { $($b1)* }
             }
-            pub fn str_len(self) -> usize {
+            pub const fn str_len(self) -> usize {
                 match self { $($b2)* }
             }
         }
     };
     (@impl
-     [def {$(#[$em:meta])*} {$vis:vis} $e:ident { $($vars:tt)* }]
+     [def {$(#[$em:meta])* $vis:vis} $e:ident { $($vars:tt)* }]
      [to_str  { $($b1:tt)* }]
      [str_len { $($b2:tt)* }] :
      $(#[$vm:meta])* $var:ident = $s:literal ,
     ) => {
         to_str!(@impl
-            [def {$(#[$em])*} {$vis} $e { $($vars)* $(#[$vm])* $var , } ]
+            [def {$(#[$em])* $vis} $e { $($vars)* $(#[$vm])* $var , } ]
             [to_str  { $($b1)* $e::$var => $s,       } ]
             [str_len { $($b2)* $e::$var => $s.len(), } ] :
         );
     };
     (@impl
-     [def {$(#[$em:meta])*} {$vis:vis} $e:ident { $($vars:tt)* }]
+     [def {$(#[$em:meta])* $vis:vis} $e:ident { $($vars:tt)* }]
      [to_str  { $($b1:tt)* }]
      [str_len { $($b2:tt)* }] :
      $(#[$vm:meta])* $var:ident = $s:literal , $($t:tt)+
     ) => {
         to_str!(@impl
-            [def {$(#[$em])*} {$vis} $e { $($vars)* $(#[$vm])* $var , } ]
+            [def {$(#[$em])* $vis} $e { $($vars)* $(#[$vm])* $var , } ]
             [to_str  { $($b1)* $e::$var => $s,       } ]
             [str_len { $($b2)* $e::$var => $s.len(), } ] :
             $($t)+
         );
     };
     (@impl
-     [def {$(#[$em:meta])*} {$vis:vis} $e:ident { $($vars:tt)* }]
+     [def {$(#[$em:meta])* $vis:vis} $e:ident { $($vars:tt)* }]
      [to_str  { $($b1:tt)* }]
      [str_len { $($b2:tt)* }] :
      $(#[$vm:meta])* $var:ident($inner:ident) ,
     ) => {
         to_str!(@impl
-            [def {$(#[$em])*} {$vis} $e { $($vars)* $(#[$vm])* $var($inner) , } ]
-            [to_str  { $($b1)* $e::$var(val) => val.to_str(), } ]
-            [str_len { $($b2)* $e::$var(val) => val.str_len(),} ] :
+            [def {$(#[$em])* $vis} $e { $($vars)* $(#[$vm])* $var($inner) , } ]
+            [to_str  { $($b1)* $e::$var(val) => val.to_str(),  } ]
+            [str_len { $($b2)* $e::$var(val) => val.str_len(), } ] :
         );
     };
     (@impl
-     [def {$(#[$em:meta])*} {$vis:vis} $e:ident { $($vars:tt)* }]
+     [def {$(#[$em:meta])* $vis:vis} $e:ident { $($vars:tt)* }]
      [to_str  { $($b1:tt)* }]
      [str_len { $($b2:tt)* }] :
      $(#[$vm:meta])* $var:ident($inner:ident) , $($t:tt)+
     ) => {
         to_str!(@impl
-            [def {$(#[$em])*} {$vis} $e { $($vars)* $(#[$vm])* $var($inner) , } ]
-            [to_str  { $($b1)* $e::$var(val) => val.to_str(), } ]
-            [str_len { $($b2)* $e::$var(val) => val.str_len(),} ] :
+            [def {$(#[$em])* $vis} $e { $($vars)* $(#[$vm])* $var($inner) , } ]
+            [to_str  { $($b1)* $e::$var(val) => val.to_str(),  } ]
+            [str_len { $($b2)* $e::$var(val) => val.str_len(), } ] :
             $($t)+
         );
     };
@@ -165,7 +206,7 @@ to_str!(
         Equal = " = ",
         /// <code> + </code>
         Plus = " + ",
-        Tick = "'",
+        Apostrophe = "'",
         AngleBracketStart = "<",
         AngleBracketEnd = ">",
         SquareBracketStart = "[",
@@ -174,6 +215,7 @@ to_str!(
         ParenthesisEnd = ")",
         BraceStart = "{",
         BraceEnd = "}",
+        Quote = "\"",
     }
 );
 
@@ -192,8 +234,8 @@ to_str!(
         /// placeholder: not showing anything
         Default = "",
         PubCrate = "pub(crate) ",
-        /// Placeholder, not showing anything. But in conjuction with [`Tag::PubScope`].
-        PubScope = "",
+        /// In conjuction with [`Tag::PubScope`].
+        PubScope = "pub",
     }
 );
 
@@ -220,8 +262,8 @@ to_str!(
         Win64 = "extern \"win64\" ",
         SysV64 = "extern \"sysv64\" ",
         System = "extern \"system\" ",
-        /// Placeholder, not showing anything. But in conjuction with [`Tag::UnusualAbi`].
-        Other = "",
+        /// In conjuction with [`Tag::UnusualAbi`].
+        Other = "extern ",
     }
 );
 

224a5bba2c0ab4ae387f030bf51af3b478b8b400
zjp <jiping_zhou@foxmail.com>
2024-03-21 16:35:19 +0800

    test: no longer test for item_str which is going to be deprecated

--- tests/parse-json-docs/fn_item_decl.rs
+++ tests/parse-json-docs/fn_item_decl.rs
@@ -1,6 +1,5 @@
 use crate::{doc, shot};
 use term_rustdoc::{
-    decl::item_str,
     tree::{DModule, IDMap},
     type_name::style::{item_styled, StyledType},
 };
@@ -10,45 +9,9 @@ fn fn_items() {
     let map = &doc();
     let dmod = map.dmodule();
     let mut styled_fn = Vec::with_capacity(24);
-    let mut fns_str = Vec::with_capacity(24);
     for fn_ in &dmod.functions {
-        fns_str.push(item_str(&fn_.id, map));
         styled_fn.push(item_styled(&fn_.id, map));
     }
-    shot!(DisplaySlice(&fns_str), @r###"
-    pub fn func_dyn_trait(d: &(dyn ATrait + Send + Sync)) -> &dyn ATrait
-    pub fn func_dyn_trait2(_: Box<dyn ATrait + Send + Sync>)
-    pub fn func_fn_pointer_impl_trait(f: fn(*mut u8) -> *const u8) -> impl Copy + Fn(*mut u8) -> *const u8
-    pub fn func_hrtb<T: ATraitWithGAT>()
-    where
-        for<'a> <T as ATraitWithGAT>::Assoc<'a>: Copy
-    pub fn func_lifetime_bounds<'a, 'b: 'a>()
-    where
-        'a: 'b
-    pub fn func_primitive(s: &str) -> usize
-    pub fn func_qualified_path<'a, I: Iterator>(iter: I) -> Option<I::Item>
-    where
-        I::Item: 'a + Debug + Iterator<Item = ()> + ATraitWithGAT<Assoc<'a> = ()>
-    pub fn func_trait_bounds<T>()
-    where
-        T: Clone + Copy
-    pub fn func_tuple_array_slice<'a, 'b>(
-        a: &'a [u8],
-        b: &'b mut [u8; 8],
-        _: &'b mut (dyn 'a + ATrait)
-    ) -> (&'a [u8], &'b mut [u8; 8])
-    pub fn func_with_1arg(_: FieldsNamedStruct)
-    pub fn func_with_1arg_and_ret(f: FieldsNamedStruct) -> AUnitEnum
-    pub fn func_with_const<T: Copy, const N: usize>(t: T) -> [T; N]
-    pub fn func_with_no_args()
-    pub unsafe extern "C" fn variadic(_: *const (), _: ...)
-    pub unsafe extern "C" fn variadic_multiline(
-        _: *const (),
-        _: *mut (),
-        _: ...
-    )
-    "###);
-
     shot!(DisplaySlice(&styled_fn), @r###"
     pub fn func_dyn_trait(d: &(dyn ATrait + Send + Sync)) -> &dyn ATrait
     pub fn func_dyn_trait2(_: Box<dyn ATrait + Send + Sync>)
